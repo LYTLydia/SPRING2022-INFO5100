9c95d15b2c695b4640b3bb98e6d192d02d256650
LYTLydia <98440303+LYTLydia@users.noreply.github.com>
2022-01-25 23:03:48 -0800

    Info 5100 assign1

---
+++ Assignment1/src/neu/lydia/Main.java
@@ -0,0 +1,50 @@
+package neu.lydia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+    }
+
+
+    //Q1
+
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+
+    //Q2
+
+    public boolean hasAllUniqueChars(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if(s.indexOf(s.charAt(i))!=s.lastIndexOf(s.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //Q3
+
+    public boolean isDuplicate(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if(s.indexOf(s.charAt(i))!=s.lastIndexOf(s.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
